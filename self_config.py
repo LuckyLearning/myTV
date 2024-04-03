27b632b5486d8a919a30451b72559928d81b7d22
LuckyLearning <lf19950801@gmail.com>
2024-04-03 18:08:06 +0800

    Create self_config.py

---
+++ self_config.py
@@ -0,0 +1,21 @@
+source_file = "self_demo.txt"
+final_file = "self_result.txt"
+favorite_list = [
+    "CCTV-1",
+    "CCTV-2",
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-8",
+    "CCTV-13",
+    "河南卫视",
+    "大湾区卫视",
+    "浙江卫视",
+    "湖南卫视",
+]
+favorite_page_num = 6
+default_page_num = 4
+urls_limit = 15
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv6"

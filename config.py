4c677f49fb5c0fd88fbe7095724746ad2e8d6cce
LuckyLearning <lf19950801@gmail.com>
2024-06-20 19:18:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,12 +6,6 @@ favorite_list = [
     "CCTV-5",
     "CCTV-5+",
     "CCTV-13",
-    "广东体育",
-    "广东卫视",
-    "大湾区卫视",
-    "浙江卫视",
-    "湖南卫视",
-    "翡翠台",
 ]
 open_online_search = True
 favorite_page_num = 5
@@ -21,7 +15,7 @@ open_sort = True
 response_time_weight = 0.5
 resolution_weight = 0.5
 recent_days = 30
-ipv_type = "ipv4"
+ipv_type = "ipv6"
 domain_blacklist = ["epg.pw"]
 url_keywords_blacklist = []
 open_subscribe = True
